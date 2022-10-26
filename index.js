79d342b10af55a2d33462848555e57fe79281d22
kmdshojib <kmd.shojib123@gmail.com>
2022-10-26 15:24:43 +0600

    data updated

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const app = express();
 app.use(cors())
 
 const courses = require('./Data/serverdata.json')
+const coursedetails = require('./Data/courseDetails.json')
 
 app.get('/',(req,res)=>{
     res.send(courses)
+})
+app.get('/coursedetails/:name',(req,res)=>{
+    const name = req.params.name
+    const seleccourse = coursedetails.find(course => course.name === name)
+    res.send(seleccourse)
+
 })
 
 const port = 5000;
